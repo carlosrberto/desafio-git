ebaf0ee32d068df9df62d4e4d16f1cfa3d38fef5
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2018-05-23 14:31:49 -0300

    added home and fix services url

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import requests
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 @app.route("/consulta-cep/")
 def consulta_cep():
