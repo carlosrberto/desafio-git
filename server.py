2bf9a12b141708cee660fcc6f34769cee108228f
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2018-05-23 14:23:35 -0300

    cep fix bug

--- server.py
+++ server.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 @app.route("/consulta-cep/")
 def consulta_cep():
     cep = request.args.get("cep")
+    if cep:
+        r = requests.get('https://viacep.com.br/ws/'+ cep +'/json/')
+        if r.status_code == 200:
+            return jsonify(r.json())
+        else: 
+            return jsonify(error='invalid cep')
+    else:
+        return jsonify(error = 'cep is  required')
     
-    r = requests.get('https://viacep.com.br/ws/'+ cep +'/json/')
-    return jsonify(r.json())
     
